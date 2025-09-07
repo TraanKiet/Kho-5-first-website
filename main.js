86f646c842e0d5bfcf8938dc8046780933afd218
TraanKiet <traankietk@gmail.com>
2025-09-07 10:54:40 +0700

    Bắt đầu có thêm javascript

---
+++ main.js
@@ -0,0 +1,4 @@
+alert("Chào mừng bạn đến với trang web của tôi!");
+console.log("Trang web đã được tải thành công.");
+var name = prompt("Vui lòng nhập tên của bạn:");
+console.log("Tên của bạn là: " + name);
